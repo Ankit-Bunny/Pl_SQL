3e96304d06cacdaa1afade74b098ff37f95f421d
Ankit-Bunny <73134844+Ankit-Bunny@users.noreply.github.com>
2023-01-27 20:53:42 +0530

    Update dml_operation_plsql.sql

--- dml_operation_plsql.sql
+++ dml_operation_plsql.sql
@@ -1,9 +1,18 @@
 -- DML Operation using PLSQL 
 -- Below code will insert same values in the store table but the primary key will be different.
+-- Inserting Statment
 begin
 for i in 2..4 loop
 insert into store(id,s_name,s_address,s_city)
 values (i,'Sanik_Sweets','Nagar Bhawan','Buxar');
+end loop;
+end;
+
+-- Updating Statement
+begin
+for i in 2..4 loop
+update store set s_city = 'Buxar - bxr'
+where id = i;
 end loop;
 end;
 
